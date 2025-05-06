b8e04a012249dc3add62e363d715709e7166f472
Francielle Magalhães <fraan2309@gmail.com>
2025-05-06 20:53:10 -0300

    Add files via upload

---
+++ Aulas Python/2.Cadastro de usuário.py
@@ -0,0 +1,16 @@
+#Programa de cadastro de usuário
+
+#Menu - cadastrar o usuário
+#def main é defirnir a função
+def main():
+    nome = ""
+    Idade = 0
+    opcao = 0
+    while True:
+        print("*MENU ENTRADA*")
+        print("1. Cadastrar o usuário")
+        print("2. Ver os dados cadastrados do usuário")
+        print("3. Sair do Sistema")
+        print("************")
+        if opcao ==
+
